5714ea4d3aa25ae8273f43d8fdca68ddadb412a2
Aleksander Litynski <ajl5242@rit.edu>
2013-01-23 00:28:04 -0500

    Fixed player camera for networking
    AJ was right. There is something wrong with the web player. I'm not sure
    what it is. Only happens if they don't remove all players before quiting
    or something. maybe they need to reenable main camera? Not sure.

--- blocker/Assets/Scripts/Managers/InputManagers/InputReceiver.cs
+++ blocker/Assets/Scripts/Managers/InputManagers/InputReceiver.cs
@@ -15,7 +15,7 @@ public class InputReceiver : BlockerObject
             {
                 player.move(new InputCollection(player, f, s, tR, tU, j, f1, f2, sp, c));
 				
-				networkView.RPC("setPlayerTransform", RPCMode.Others, player.transform.position, player.transform.rotation.eulerAngles, player.playerArms.rotation.eulerAngles.x, player.name);
+				networkView.RPC("setPlayerTransform", RPCMode.Others, player.transform.position, player.transform.rotation.eulerAngles, player.playerArms.rotation, player.name);
                 break;
             }
         }
@@ -27,14 +27,14 @@ public class InputReceiver : BlockerObject
     }
 	
 	[RPC]
-	public void setPlayerTransform(Vector3 pos, Vector3 rot, float armRot, string playerName)
+	public void setPlayerTransform(Vector3 pos, Vector3 rot, Quaternion armRot, string playerName)
 	{
 		foreach (NetPlayer player in playerManager.players)
         {
             if (player.name == playerName)
             {
 				player.setTransform(pos, rot);
-				player.playerArms.rotation = Quaternion.Euler(armRot, player.playerArms.rotation.eulerAngles.y, player.playerArms.rotation.eulerAngles.z);
+				player.playerArms.rotation = armRot;
                 break;
             }
         }
