de7e20c244472878fb8280995bb98919789fb4e2
Anthony DiStefano <ajd5635@g.rit.edu>
2013-01-22 20:23:32 -0500

    Stuff works
    In professional terms, the connection manager now communicates witht he
    master server to make a list of servers available, without showing the
    host IP

--- blocker/Assets/Scripts/Managers/ConnectionManager.cs
+++ blocker/Assets/Scripts/Managers/ConnectionManager.cs
@@ -3,21 +3,88 @@ using System.Collections.Generic;
 
 public class ConnectionManager : BlockerObject 
 {
-	public string serverAddress = "bigwhite.student.rit.edu";
+	//public string serverAddress = "bigwhite.student.rit.edu";
+	string gameName = "Blocker v 1.0.134523576";
 
     void OnGUI()
     {
         if (Network.peerType == NetworkPeerType.Disconnected)
         {
-            GUILayout.Window(0, new Rect(0, 0, 200, 140), drawWindow, "Connection Settings");
+            GUILayout.BeginHorizontal();
+				ServerGUI();
+				ClientGUI();
+			GUILayout.EndHorizontal();
         }
         else
         {
-            GUILayout.Window(0, new Rect(0, 0, 200, 80), drawWindow, "Connection Settings");
+            GUILayout.BeginHorizontal();
+				DisconnectGUI();
+			GUILayout.EndHorizontal();
         }
 	}
+	
+	void DisconnectGUI()
+	{
+		
+		if(GUI.Button(new Rect(0, 0, 120, 20), "Return to Lobby"))
+		{
+			Network.Disconnect();
+		}
+		
+	}
+	
+	
+	string hostedGameName = "Default Game Name";
+	string hostedGameDescription = "Default Description";
+	void ServerGUI()
+	{
+		Debug.Log("Is Called");
+		
+		GUILayout.BeginVertical();
+			hostedGameName = GUILayout.TextField(hostedGameName, GUILayout.MinWidth (100));
+			hostedGameDescription = GUILayout.TextField(hostedGameDescription, GUILayout.MinWidth(200));
+			if(GUILayout.Button ("Host", GUILayout.MinWidth(50)))
+			{
+				createServer(hostedGameName, hostedGameDescription);
+			}
+		GUILayout.EndVertical();
+		
+	}
 
-    void drawWindow(int winID)
+ 	void createServer(string hostName, string description)
+	{
+		Network.InitializeServer(32, Random.Range(2000,40000), !Network.HavePublicAddress());
+		MasterServer.RegisterHost(gameName, hostName, description);
+		
+	}
+	
+	void ClientGUI()
+	{
+		MasterServer.RequestHostList(gameName);
+		GUILayout.BeginVertical();
+		HostData[] availableHosts = MasterServer.PollHostList();
+			GUILayout.Box("Available Servers", GUILayout.MinWidth(Screen.width));
+			foreach (HostData host in availableHosts)
+			{
+				connectionRow(host);
+			}
+		GUILayout.EndVertical();
+		
+	}
+	
+	void connectionRow(HostData host)
+	{
+		GUILayout.BeginHorizontal();
+			GUILayout.Label(host.gameName + " (" + host.connectedPlayers + " / " + host.playerLimit + ")");
+			GUILayout.Label(host.comment);
+			if (GUILayout.Button("Connect", GUILayout.MaxWidth(Screen.width/10)))
+			{
+				Network.Connect(host);			
+			}				
+		GUILayout.EndHorizontal();
+	}
+	
+	/*void drawWindow(int winID)
     {
         if (Network.peerType == NetworkPeerType.Disconnected)
         {
@@ -40,7 +107,7 @@ public class ConnectionManager : BlockerObject
                 Network.Disconnect();
             }
         }
-    }
+    }*/
 	
 
 
