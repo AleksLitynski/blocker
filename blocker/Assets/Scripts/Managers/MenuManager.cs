6c56a739783357bc8c377e83a77434f7ec2379ac
mike <mcl6832@rit.edu>
2013-02-06 11:25:15 -0500

    Better lobby system

--- blocker/Assets/Scripts/Managers/MenuManager.cs
+++ blocker/Assets/Scripts/Managers/MenuManager.cs
@@ -151,11 +151,15 @@ public class MenuManager : BlockerObject
 			
 			break;
 		case GameState.Lobby:
-			GUILayout.BeginArea(new Rect(Screen.width*1/6, Screen.height*1/6, Screen.width*2/3, Screen.height*2/3));
+			/*GUILayout.BeginArea(new Rect(Screen.width*1/6, Screen.height*1/6, Screen.width*2/3, Screen.height*2/3));
 				GUILayout.BeginVertical();	
 					// list all players in the lobby.
 					foreach(NetPlayer player in playerManager.players)
 					{
+						if(player.networkPlayer.ToString() == Network.player.ToString())
+						{
+							
+						}
 						GUILayout.Label(player.gameObject.name);
 					}
 				GUILayout.EndVertical();
@@ -178,10 +182,81 @@ public class MenuManager : BlockerObject
 						}
 					}
 				GUILayout.EndHorizontal();
-			
-				
 			GUILayout.EndArea();
-				playerAddRemove();
+			playerAddRemove();*/
+			
+			GUILayout.BeginArea(new Rect(0,0,Screen.width, Screen.height));
+				GUILayout.BeginHorizontal();
+					// provide add/drop ability
+					GUILayout.BeginArea(new Rect(Screen.width*1/20,Screen.height*1/20,Screen.width*2/20, Screen.height*15/20));
+						GUILayout.BeginVertical();
+						if (GUILayout.Button("New Player", GUILayout.MinWidth(Screen.width*2/20)))
+				        {
+				            if (Network.peerType == NetworkPeerType.Client) networkView.RPC("AddNewPlayerRequest", RPCMode.Server);
+				            if (Network.peerType == NetworkPeerType.Server) playerManager.AddNewPlayerRequest(new NetworkMessageInfo());
+				        }
+						foreach(NetPlayer player in playerManager.players)
+						{
+							if(player.networkPlayer.ToString() == Network.player.ToString())
+							{
+								if (GUILayout.Button("Drop", GUILayout.MinWidth(Screen.width*2/20)))
+					            {
+					                networkView.RPC("RemovePlayerRequest", RPCMode.Server, player.localPlayerNumber);
+					                if (Network.peerType == NetworkPeerType.Server) playerManager.RemovePlayerRequest(player.localPlayerNumber, new NetworkMessageInfo());
+					            }
+							}
+							else
+							{
+								GUILayout.Label("");
+							}
+						}
+						GUILayout.EndVertical();
+					GUILayout.EndArea();
+					// list player names
+					GUILayout.BeginArea(new Rect(Screen.width*4/20,Screen.height*1.9f/20,Screen.width*5/20, Screen.height*14/20));
+						foreach(NetPlayer player in playerManager.players)
+						{
+							GUILayout.Label(player.gameObject.name);
+						}
+					GUILayout.EndArea();
+					// list player scores
+					GUILayout.BeginArea(new Rect(Screen.width*10/20,Screen.height*1.9f/20,Screen.width*5/20, Screen.height*14/20));
+						foreach(NetPlayer player in playerManager.players)
+						{
+							GUILayout.Label("" + player.playerStats.score);
+						}
+					GUILayout.EndArea();
+				GUILayout.EndHorizontal();
+				GUILayout.BeginArea(new Rect(Screen.width*(1.0f/6), Screen.height*17/20, Screen.width*2/3, Screen.height*2/20));
+					GUILayout.BeginHorizontal();
+						// actually initialize the game state.
+						if (GUILayout.Button("Start", GUILayout.MaxWidth(200)))
+						{
+							if (Network.peerType == NetworkPeerType.Server)
+							{
+								// buffer an RPC telling everyone the game has started (join in progress)
+								networkView.RPC("ChangeState", RPCMode.AllBuffered, GameCode);
+								networkView.RPC("initializeGame", RPCMode.All);
+							}
+						}
+						GUILayout.Label("", GUILayout.MaxWidth(Screen.width*2/3-400));
+						if (GUILayout.Button("Back to Main Menu", GUILayout.MaxWidth(200)))
+						{
+							if(Network.peerType == NetworkPeerType.Server)
+							{
+								// return yourself to the main menu and everyone else to the joingame menu.
+								ChangeState (GameState.MainMenu);
+								networkView.RPC("ChangeState", RPCMode.Others, JoinGameCode);
+								MasterServer.UnregisterHost();
+							}
+							else
+							{
+								Network.Disconnect();	
+							}
+						}
+					GUILayout.EndHorizontal();
+				GUILayout.EndArea();
+			GUILayout.EndArea();
 			break;
 		case GameState.Game:
 			// manage a menu for each local player (placement and settings)
@@ -195,7 +270,7 @@ public class MenuManager : BlockerObject
 				cameraZone.height = Screen.height * cameraZone.height;
 				
 				GUILayout.BeginArea(cameraZone);
-					GUILayout.TextArea("score: " + player.playerStats.score);
+					GUILayout.Label("score: " + player.playerStats.score);
 				GUILayout.EndArea();
 			}
 			
